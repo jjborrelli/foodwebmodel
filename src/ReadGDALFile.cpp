1ffe52aef47a8b566b8959bec8eec71adca93a46
Manu Garcia <mgarcia.universidad@gmail.com>
2017-03-22 12:52:13 -0400

    Starting to read the .bil raster file.

--- src/ReadGDALFile.cpp
+++ src/ReadGDALFile.cpp
@@ -1,13 +1,46 @@
 #include "../headers/FoodWebModel.hpp"
-
+#include <iostream>
 using namespace std;
 
 void FoodWebModel::ReadGDALFile::readGDALFile(string filename){
 	GDALAllRegister();
 
+	/*Read raster data*/
 	 poDataset = (GDALDataset *) GDALOpen(filename.c_str(), GA_ReadOnly );
-	    if( poDataset == NULL )
+	    if( poDataset != NULL )
 	    {
-	    	int raster_x_size = GDALGetRasterXSize(poDataset);
+	    	/*Get raster dimensions*/
+	    	int raster_x_size = GDALGetRasterXSize(poDataset), raster_y_size = GDALGetRasterYSize(poDataset);
+	    	int numberOfBands = poDataset->GetRasterCount() ;
+
+	    	/*Fetch the raster band (presumably 1) and read its data*/
+	    	GDALRasterBand* poBand = poDataset->GetRasterBand( 1 );
+	    	int band_x_size = poBand->GetXSize(), band_y_size = poBand->GetYSize();
+
+	    	/*Get block size*/
+	    	int nXBlockSize, nYBlockSize;
+	    	poBand->GetBlockSize( &nXBlockSize, &nYBlockSize );
+	    	int nXBlocks = (poBand->GetXSize() + nXBlockSize - 1) / nXBlockSize;
+	    	int nYBlocks = (poBand->GetYSize() + nYBlockSize - 1) / nYBlockSize;
+
+	    	 GByte *pabyData = (GByte *) CPLMalloc(nXBlockSize * nYBlockSize);
+	    	     for( int iYBlock = 0; iYBlock < nYBlocks; iYBlock++ )
+	    	     {
+	    	         for( int iXBlock = 0; iXBlock < nXBlocks; iXBlock++ )
+	    	         {
+	    	             int        nXValid, nYValid;
+	    	             CPLErr error_block_signal = poBand->ReadBlock( iXBlock, iYBlock, pabyData );
+	    	             // Compute the portion of the block that is valid
+	    	             // for partial edge blocks.
+
+	    	         }
+	    	     }
+	    	float* pafScanline = (float *) CPLMalloc(sizeof(float)*band_x_size*band_y_size);
+	    	CPLErr error_band_signal = poBand->RasterIO( GF_Read, 0, 0, band_x_size, band_y_size,
+	    	                  pafScanline, band_x_size, band_y_size, GDT_Float32,
+	    	                  0, 0 );
+
+	    	cout << "X: "<<raster_x_size<<", Y: "<<raster_y_size<<", number: "<<numberOfBands;
+
 	    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,16 @@
 #include <string>
 #include <cstdlib>
 using namespace std;
+using namespace FoodWebModel;
 
 
 int main(int argc, char **argv) {
 	FoodWebModel::FoodWebModel foodWebModel;
-	string outputFileName = argv[1];
-	int simulationCycles=atoi(argv[2]);
+	string bathymetryFile = argv[1];
+	string outputFileName = argv[2];
+	int simulationCycles=atoi(argv[3]);
+	ReadGDALFile fileReader;
+	fileReader.readGDALFile(bathymetryFile);
 	//foodWebModel.simulate(simulationCycles, outputFileName.c_str());
 }
 

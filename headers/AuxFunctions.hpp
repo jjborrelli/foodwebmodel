690a429522af43f59975adc7e818ffe2e1a889ff
Manu Garcia <mgarcia.universidad@gmail.com>
2017-07-11 18:30:05 -0400

    Added predator dynamics attribute

--- headers/AuxFunctions.hpp
+++ headers/AuxFunctions.hpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * AuxFunctions.hpp
  *
@@ -17,23 +17,3 @@ vector<string> split(string &str);
 
 
 #endif /* AUXFUNCTIONS_HPP_ */
-=======
-/*
- * AuxFunctions.hpp
- *
- *  Created on: Jul 11, 2017
- *      Author: manu_
- */
-
-#ifndef AUXFUNCTIONS_HPP_
-#define AUXFUNCTIONS_HPP_
-
-#include <vector>
-#include <string>
-using namespace std;
-vector<string> generalSplit(const string& str, const string& delim);
-vector<string> split(string &str);
-
-
-#endif /* AUXFUNCTIONS_HPP_ */
->>>>>>> 3db54f91f839b1fd8f4d8bde6dd74c1b6240b691
